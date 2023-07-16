cf43e038b683682fd6f29c76bc672b96c9887bbc
Mr.RySes <mpoprev2@email.com>
2023-07-16 12:21:16 +0800

    07-16-23 12:21

--- background.js
+++ background.js
@@ -1,15 +1,20 @@
-chrome.browserAction.onClicked.addListener(() => {
-	var w = 500;
-	var h = 350;
-	var left = (screen.width/2)-(w/2);
-	var top = (screen.height/2)-(h/2); 
+chrome.commands.onCommand.addListener((command) => {
+	switch(command){
+		case "baybayin":
+			let w = 500;
+			let h = 350;
+			// var left = (screen.width/2)-(w/2);
+			// var top = (screen.height/2)-(h/2); 
 
-	chrome.windows.create({
-		'url': 'index.html',
-		'type': 'popup',
-		'width': w,
-		'height': h,
-		'left': left,
-		'top': top
-	} , (window) => {})
+			console.log(command)
+			chrome.windows.create({
+				'url': 'index.html',
+				'type': 'popup',
+				'width': w,
+				'height': h,
+				'left': 100,
+				'top': 100
+			} , (window) => {})
+		break
+	}
 })
